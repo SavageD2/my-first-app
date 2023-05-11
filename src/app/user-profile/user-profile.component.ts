d91c082cf8b8ceb6e131a42574ab753078c03403
Marvin <bourhanemarvin@hotmail.fr>
2023-05-11 15:43:21 +0200

    user profile works

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit {
+
+  user = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : '',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
